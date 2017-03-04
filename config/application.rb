d1002cfebe26890dda075e5c498f29d8e67e92b2
shimpeiws <shimpeiws@gmail.com>
2017-03-04 12:32:56 +0900

    Allow CORS

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module SimpleRailsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => 'http://localhost:3355',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
