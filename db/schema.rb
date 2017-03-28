38839ff35742b908cda540d822d686f7cc98345e
shimpeiws <shimpeiws@gmail.com>
2017-03-29 02:10:44 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304002631) do
+ActiveRecord::Schema.define(version: 20170328170701) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "body"
+    t.index ["item_id"], name: "index_comments_on_item_id", using: :btree
+  end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -26,4 +40,5 @@ ActiveRecord::Schema.define(version: 20170304002631) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "comments", "items"
 end
